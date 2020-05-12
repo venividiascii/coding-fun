8ab557aff197494fffd71ff673a859ba030931ba
nathangineer <osrevad@gmail.com>
2020-05-11 17:46:34 -0700

    FIrst working game

--- 2020-05-11-basic-15-puzzle/board.js
+++ 2020-05-11-basic-15-puzzle/board.js
@@ -6,7 +6,7 @@ class Board{
 		this.tileArr.push(this.tileList.slice(4, 8 ))
 		this.tileArr.push(this.tileList.slice(8, 12))
 		this.tileArr.push(this.tileList.slice(12,16))
-		this.emptyPos = { x : 3, y : 3 }
+		this.emptyPos = createVector(3, 3)
 	}
 	shuffleBoard(){
 		let list = [
@@ -25,41 +25,41 @@ class Board{
 	}
 	legalMoves(){
 		let moveList = [];
-		if (this.emptyPos.x > 0) { moveList.push("LEFT")  }
-		if (this.emptyPos.x < 3) { moveList.push("RIGHT") }
-		if (this.emptyPos.y > 0) { moveList.push("UP")    }
-		if (this.emptyPos.y < 3) { moveList.push("DOWN")  }		
+		if (this.emptyPos.x > 0) { moveList.push("RIGHT")}
+		if (this.emptyPos.x < 3) { moveList.push("LEFT") }
+		if (this.emptyPos.y > 0) { moveList.push("DOWN") }
+		if (this.emptyPos.y < 3) { moveList.push("UP")   }		
 		return moveList;
 	}
 	moveTile(moveDir) {
 		if ( this.legalMoves().includes(moveDir) ) {
-			let slideTile = this.emptyPos;
+			let slideTile = createVector(this.emptyPos.x, this.emptyPos.y);
 			//console.log(slideTile);
 			switch (moveDir) {
 				case "LEFT":
 					console.log(slideTile);
-					slideTile.x -= 1;
+					slideTile.x += 1;
 					console.log(slideTile);
 					break;
 				case "RIGHT":
-					slideTile.x += 1;
+					slideTile.x -= 1;
 					break;
 				case "UP":
-					slideTile.y -= 1;
-					break;
-				case "DOWN":
 					slideTile.y += 1;
 					break;
+				case "DOWN":
+					slideTile.y -= 1;
+					break;
 			}
+
+			
+			let tempTile = this.tileArr[slideTile.x][slideTile.y];
 			console.log(slideTile);
 			console.log(this.emptyPos)
-			this.emptyPos = slideTile
-			let tempTile = this.tileArr[slideTile.x][slideTile.y];
-			console.table(this.tileArr);
-			
 			this.tileArr[slideTile.x][slideTile.y] = this.tileArr[this.emptyPos.x][this.emptyPos.y];
-						console.table(this.tileArr);
+			console.table(this.tileArr);
 			this.tileArr[this.emptyPos.x][this.emptyPos.y] = tempTile;
+			this.emptyPos = slideTile
 		}
 	}
 	swapTile(swap) {
@@ -73,11 +73,11 @@ class Board{
 		//Probably need to swap
 		for (let i = 0 ; i < this.tileArr.length ; i++){
 			for (let j = 0 ; j < this.tileArr[0].length ; j++){
-				if (this.tileArr[j][i] != " ") {
+				if (this.tileArr[i][j] != " ") {
 					fill(180);
 					rect(i*tileSize, j*tileSize, tileSize, tileSize);
 					fill(30);
-					text(this.tileArr[j][i], 
+					text(this.tileArr[i][j], 
 					i*tileSize + tileSize/2, j*tileSize + tileSize/2);
 				}
 			}

b7f75163cad576d6bccda16b12631cf23225f700
nathangineer <osrevad@gmail.com>
2020-05-18 19:44:12 -0700

    Isometric elevation
    Final planned version. It can still generate

--- 2020-05-17 Iso Elevation Map/main.js
+++ 2020-05-17 Iso Elevation Map/main.js
@@ -1,33 +1,99 @@
+'use strict'
 const WIDTH = 600;
 const HEIGHT = 600;
 
-//load image
-//load pixels
-//swap pixels at position
-//push pixels
+const tileW = 100;
+const tileH = 50
+
+// figure out how to render a single tile based on height maps
+
+//  1 __ 2      1 
+//   |  |      /\ 2
+//   |__|    4 \/
+//  4    3     3
+
+//const tileArr = [
+//  [26, 30, 20],
+//  [1, 1, 8],
+//  [4, 3, 7]
+//]
+
+const tileArr = [
+  [-20, -20, -20, -20, -20],
+  [1, 1, 1, 1, -10],
+  [1, 9, 1, 1, 30],
+  [10, 50, 60, 80, 80],
+  [15, 50, 60, 80, 80]
+]
+
+let tileMap;
+
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-  background(0)
-  fill(255)
-  rect(0, 0, width/2, height)
-  frameRate(500)
+  tileMap = subSquares(tileArr);
+  //console.table(tileMap)
 }
 
 function draw() {
   //background(30);
-  loadPixels();
-  
-  for (let i = 0; i < 100000; i++){
-    const offset = floor(random(20))
-    let randX1 = floor(random(width)) + offset
-    let randY1 = floor(random(height)*2)*width + offset
-    //let randX2 = floor(random(width)) + offset
-    //let randY2 = floor(random(height)*2)*width + offset
-    
-    let temp = pixels[randX1 + randY1]
-    pixels[randX1 + randY1] = pixels[randX1 + randY1 + 4]
-    pixels[randX1 + randY1 + 4] = temp
+
+  //renderTile(tileArr, 1, 2);
+  let i = 0;
+
+  for (let i = 0; i < tileMap.length; i++) {
+    for (let j = 0; j < tileMap[0].length; j++) {
+      console.table(tileMap[i][j])
+      renderTile(tileMap[j][i], i, j)
+    }
   }
-  
-  updatePixels();
+  noLoop()
+}
+
+// takes a large 2D array, and returns an array
+// where each tile is a flattened list of the array
+function subSquares(tileMap) {
+  let tileArray = []
+  for (let i = 0; i < tileMap.length - 1; i++) {
+    let tileRow = []
+    for (let j = 0; j < tileMap.length - 1; j++) {
+      tileRow.push([
+        tileMap[i][j],
+        tileMap[i + 1][j],
+        tileMap[i + 1][j + 1],
+        tileMap[i][j + 1]
+      ])
+    }
+    tileArray.push(tileRow)
+  }
+  return tileArray;
+}
+
+function renderTile(tile, xPos, yPos) {
+  //transform points
+  //if (yPos % 2 == 1) {xPos += .5}
+  let tilePosX = 100 + (tileW/2) * xPos + tileW * yPos / 2;
+  let tilePosY = 200 + tileH * yPos/2 - (tileH/2) * xPos;
+  console.table(tile)
+  let p4 = tile[0]
+  let p3 = tile[1]
+  let p2 = tile[2]
+  let p1 = tile[3]
+
+  //level square case
+
+  beginShape();
+  vertex(tilePosX + tileW / 2,
+    p1 + tilePosY + 0)
+  vertex(tilePosX + tileW,
+    p2 + tilePosY + tileH / 2)
+  vertex(tilePosX + tileW / 2,
+    p3 + tilePosY + tileH)
+  vertex(tilePosX + 0,
+    p4 + tilePosY + tileH / 2)
+  endShape(CLOSE);
+
+  //tilted square case
+
+
+  //folder square
 }
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # coding fun
  A series of experimental projects, bundled together in one nice messy package,
+
+# notable projects
+
+I'm going to currate a list of the more interesting projects here
+
+# future projects
+Projects that I may or may not code in the future.
+- use matter.js to create a bouncy wheel game
+- make a wolfenstein style clone, but in the map editor allow placement of light sources that will be baked into ray-traced lighting for the level.
+- a purely prime-factor based number system.
+-- an exploration for what that even means
+-- prime factors baked into the symbology itself
+-- a viewer to explore large images like Google Maps (scroll to zoom, drag to pan)
+-- animated julia set fractal viewer. Loops with perlin noise? Full screen 1080p
